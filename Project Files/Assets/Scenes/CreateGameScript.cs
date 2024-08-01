05eeee67e86a798a64f701737b123b850466eede
TalGordin <talgordin7@gmail.com>
2024-08-01 10:36:40 +0300

    Worked on Create and Join Game

--- Project Files/Assets/Scenes/CreateGameScript.cs
+++ Project Files/Assets/Scenes/CreateGameScript.cs
@@ -3,23 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
+using Photon.Realtime;
 using TMPro;
 
 public class CreateGameScript : MonoBehaviourPunCallbacks
 {
     public string DestinationScene;
     public string GameID;
+    public int MaxPlayers;
+
+    void Start()
+    {
+        // Connect to the Photon Master Server
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Master Server.");
+    }
 
     public void CreateGameRoom()
     {
-        GameID = System.Guid.NewGuid().ToString();
-        Debug.Log($"Successfully generated game ID: {GameID}");
+        if (PhotonNetwork.IsConnectedAndReady)
+        {
+            GameID = System.Guid.NewGuid().ToString();
+            GameID = GameID.Substring(0, 4).ToUpper();
+            Debug.Log($"Successfully generated game ID: {GameID}");
 
-        PhotonNetwork.CreateRoom(GameID);
+            RoomOptions roomOptions = new RoomOptions();
+            roomOptions.MaxPlayers = MaxPlayers;
+            roomOptions.CustomRoomProperties = new ExitGames.Client.Photon.Hashtable { { "GameID", GameID } };
+            roomOptions.CustomRoomPropertiesForLobby = new string[] { "GameID" }; // Include GameID in the lobby property list if needed
+
+            PhotonNetwork.CreateRoom(GameID, roomOptions);
+        }
+        else
+        {
+            Debug.LogError("Cannot create room. Not connected to the Master Server.");
+        }
     }
 
     public override void OnJoinedRoom()
     {
-        PhotonNetwork.LoadLevel("DestinationScene");
+        PhotonNetwork.LoadLevel(DestinationScene);
     }
 }
