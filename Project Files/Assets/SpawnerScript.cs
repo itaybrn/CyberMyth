405711c64366923838b806cd0dca429db5bd74e5
TalGordin <talgordin7@gmail.com>
2024-08-18 12:48:07 +0300

    Added second game level

--- Project Files/Assets/SpawnerScript.cs
+++ Project Files/Assets/SpawnerScript.cs
@@ -7,7 +7,7 @@ public class SpawnerScript : MonoBehaviour
     {
         if (PhotonNetwork.IsConnectedAndReady) // Check if connected to Photon
         {
-            Vector3 spawnPosition = new Vector3(Random.Range(4.5f, 16), 15.5f, 0);
+            Vector3 spawnPosition = new Vector3(Random.Range(65.5f, 95), 15.5f, 0);
             PhotonNetwork.Instantiate("CollegeStudent/Demo/CollegeStudent Variant", spawnPosition, Quaternion.identity);
 
             Debug.Log($"Player created for {PhotonNetwork.LocalPlayer.NickName}");
